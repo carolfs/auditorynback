6eac586fc8cc3e02e11dbfa5db7a641686a38b0f
Carolina Feher da Silva <carolfsu@gmail.com>
2023-10-30 12:11:53 +0000

    Trying to add controls

--- nback.js
+++ nback.js
@@ -25,7 +25,56 @@ const letters = ['a', 'h', 'j', 'l', 'm', 'o', 'q', 'r', 's', 't', 'u', 'w', 'x'
 
 var running = false;
 var threshold = 0.8;
-const n = 2;
+var n = 2;
+
+function updatelevel() {
+    const levelfield = document.getElementById("tasklevel");
+    const newlevel = Number(levelfield.value);
+    if (isNaN(newlevel) || newlevel < 1 || newlevel > 4) {
+        levelfield.value = 2;
+        n = 2;
+    }
+    else {
+        n = newlevel;
+    }
+}
+
+function updatethreshold() {
+    const thresholdfield = document.getElementById("threshold");
+    const newthreshold = Number(thresholdfield.value);
+    if (isNaN(newthreshold) || newthreshold < 0 || newthreshold > 100) {
+        thresholdfield.value = 80;
+        threshold = 0.8;
+    }
+    else {
+        threshold = 0.01*newthreshold;
+    }
+}
+
+async function testrecord() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const recordbutton = document.getElementById("recordbutton");
+    const recordresults = document.getElementById("recordresults");
+    recordbutton.disabled = true;
+    recordresults.innerHTML = "";
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+    const chunkpromise = new Promise((resolve, _) => {
+        mediaRecorder.addEventListener("dataavailable", (event) => resolve(event.data));
+    });
+    const blobpromise = new Promise((resolve, _) => {
+        mediaRecorder.addEventListener("stop", () =>
+            resolve(new Blob(audioChunks, { type: 'audio/wav' })));
+    });
+    mediaRecorder.start();
+    setTimeout(() => mediaRecorder.stop(), 2000)
+    const chunk = await chunkpromise;
+    audioChunks.push(chunk);
+    const audioBlob = await blobpromise;
+    const magnitude = await getmagnitude(audioBlob);
+    recordresults.innerHTML = (magnitude > threshold) ? "RESPONSE" : "NO RESPONSE";
+    recordbutton.disabled = false;
+}
 
 async function runtask() {
     running = true;
@@ -64,7 +113,7 @@ async function runtask() {
             setTimeout(resolve, 700);
         });
 
-        // Record sound (2000 ms)
+        // Record sound
         const mediaRecorder = new MediaRecorder(stream);
         const audioChunks = [];
         const chunkpromise = new Promise((resolve, _) => {
